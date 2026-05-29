76dd87e7811f71bab2f466afdf5fd08f54409e9a
Humbert1234 <humbertchris4@gmail.com>
2026-05-29 13:24:29 +0100

    Updated

--- app/components/DatadogInit.tsx
+++ app/components/DatadogInit.tsx
@@ -11,9 +11,9 @@ export default function DatadogInit() {
     initialized = true;
 
     datadogRum.init({
-      applicationId: '419947f4-8e4e-4e30-a97d-a476b3df85a9',
-      clientToken: 'pubd99550e7fdb656e6c780f0533ed3831c',
-      site: 'us5.datadoghq.com',
+      applicationId: process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID ?? '',
+      clientToken: process.env.NEXT_PUBLIC_CLIENT_TOKEN ?? '',
+      site: process.env.NEXT_PUBLIC_DATADOG_SITE ?? '',
       service: 'medchat',
       env: process.env.NODE_ENV,
       version: '1.0.0',
